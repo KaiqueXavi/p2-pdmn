e6afc6ca7b72b0e10bee79b16b085348c3974740
Kaique.Xavier <Kaiquecavalcantexavier@gmail.com>
2025-06-06 23:48:27 -0300

    feat(p2): cria a barra de busca no app

--- p2-PDMN/src/App.jsx
+++ p2-PDMN/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
+import { InputText } from 'primereact/inputtext'
 
 const App = () => {
+  const [termoDeBusca, setTermoDeBusca] = useState('')
+
   return (
-    <div>
-      <h1>Hello p2</h1>
+    <div className='border-bottom border border-1 border-400 p-2 text-center font-bold'>
+        <IconField iconPosition='left'>
+        <InputIcon className='pi pi-search'/>
+        <InputText 
+        className='border-bottom border border-1 border-400 font-bold'
+        placeholder='Buscar...'
+        onChange={(e) => {setTermoDeBusca(e.target.value)}}
+        value={termoDeBusca} />
+        </IconField> 
+      
     </div>
   )
 }
